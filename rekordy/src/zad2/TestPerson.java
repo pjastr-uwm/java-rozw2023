b54ef592bf0e8c52d48353e975da655d5e4b82c6
pjastr <24464854+pjastr@users.noreply.github.com>
2023-12-18 23:38:51 +0100

    rekordy 1-3

---
+++ rekordy/src/zad2/TestPerson.java
@@ -0,0 +1,13 @@
+package zad2;
+
+public class TestPerson {
+
+    public static void main(String[] args) {
+        Address address = new Address("Kwiatowa", "1", "00-100", "Warszawa");
+        Person person = new Person("Jan", "Kowalski", address);
+        System.out.println(person);
+        Person person2 = new Person("Jan", "Kowalski", new Address("Kwiatowa", "1", "00-100", "Warszawa"));
+        System.out.println(person2);
+        System.out.println(person.equals(person2));
+    }
+}
